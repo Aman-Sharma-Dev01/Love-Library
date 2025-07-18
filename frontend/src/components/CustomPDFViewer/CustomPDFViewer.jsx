7a946378270b791af3b6142712705083e6f96ec9
Aman Sharma <asharma8464@gmail.com>
2025-07-18 17:31:51 +0530

    zoom fix

--- frontend/src/components/CustomPDFViewer/CustomPDFViewer.jsx
+++ frontend/src/components/CustomPDFViewer/CustomPDFViewer.jsx
@@ -143,7 +143,7 @@ const [tooltipPos, setTooltipPos] = useState({ x: 0, y: 0 });
             <Page
               pageNumber={i + 1}
               data-page-number={i + 1}
-              width={Math.min(window.innerWidth * 0.65, 650)}
+              width={Math.min(window.innerWidth * 0.8, 800)}
               renderMode="canvas"
               renderAnnotationLayer={true}
               renderTextLayer={true}
